d35d9133104dded86b39502aa8467f883ce8c3be
ajsalsalim <ajsalsalim251@gmail.com>
2023-10-25 08:58:23 +0530

    loadingdemo commit

--- Frontend/src/Components/Common/HomeBody.js
+++ Frontend/src/Components/Common/HomeBody.js
@@ -5,15 +5,17 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
+import CircularProgress from '@mui/joy/CircularProgress';
 import Grid from '@mui/joy/Grid';
 
 const HomeBody = () => {
   const [professionlist,setProfessionlist]=useState([]);
-
+  const [loading,setLoading]=useState(true)
 
   useEffect(()=>{
     axios.get(`${process.env.REACT_APP_API_SERVER_URL}/professions`)
    .then((data)=>{
+    setLoading(false)
      console.log(data.data.professionlist);
      setProfessionlist(data.data.professionlist)
    })
@@ -25,6 +27,7 @@ const HomeBody = () => {
   return (
     <>
    <div style={{ marginTop: "40px" }}>
+    {loading&& <CircularProgress variant="outlined" />}
    <Grid
       container
       spacing={{ xs: 2, md: 3 }}
